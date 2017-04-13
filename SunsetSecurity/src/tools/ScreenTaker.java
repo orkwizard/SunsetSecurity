23121e990074bf8b19dec1a5a43933a88c578d4c
EdgarOsorio <EdgarOsorio@10.194.24.103>
2017-04-13 13:03:15 -0500

    Initial Code

--- SunsetSecurity/src/tools/ScreenTaker.java
+++ SunsetSecurity/src/tools/ScreenTaker.java
@@ -1,5 +1,36 @@
 package tools;
 
-public class ScreenTaker {
+import java.awt.image.BufferedImage;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
+public class ScreenTaker {
+	
+	private String IP;
+	private String HostName;
+	
+	
+	public ScreenTaker(){
+		try {
+			IP = Inet4Address.getLocalHost().getHostAddress();
+			HostName = Inet4Address.getLocalHost().getHostName();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public boolean captureLocal(){
+		
+		return true;
+	}
+
+	public BufferedImage capture(){
+		
+		
+		return null;
+	}
 }
