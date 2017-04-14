9b78281a2efcf1e4c816a1d76f2d788720571f43
edgarosorio <edgarosorio@192.168.0.101>
2017-04-13 22:31:55 -0500

    New classes

--- SunsetSecurity/src/pojo/Screen.java
+++ SunsetSecurity/src/pojo/Screen.java
@@ -8,6 +8,31 @@ public class Screen {
 	private String IP;
 	private String HostName;
 	private BufferedImage image;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getIP() {
+		return IP;
+	}
+	public void setIP(String iP) {
+		IP = iP;
+	}
+	public String getHostName() {
+		return HostName;
+	}
+	public void setHostName(String hostName) {
+		HostName = hostName;
+	}
+	public BufferedImage getImage() {
+		return image;
+	}
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+	
 	
 	
 }
